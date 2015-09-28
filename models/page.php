057280843e48d20cceb0002ab9064a08fb675a65
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-28 16:56:29 +0300

    default.html

--- models/page.php
+++ models/page.php
@@ -90,35 +90,4 @@ class Page extends Model{
     }
 
 
-    // Send message
-    public function SendMsg($data, $id = null){
-        if ( !isset($data['name']) || !isset($data['phone']) ){
-            return false;
-        }
-
-        $id = (int)$id;
-        $name = $this->db->escape($data['name']);
-        $phone = $this->db->escape($data['phone']);
-
-        if ( !$id ){ // Add new record
-            $sql = "
-                insert into `messages`
-                   set name = '{$name}',
-                       phone = '{$phone}'
-            ";
-        } else { // Update existing record
-            $sql = "
-                update `messages`
-                   set name = '{$name}',
-                       phone = '{$phone}'
-                   where id = {$id}
-            ";
-        }
-
-
-
-        return $this->db->query($sql);
-
-    }
-
 }
