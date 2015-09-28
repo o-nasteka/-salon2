ba0a27ad9725998c740f9064798614acd3013af3
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-28 18:52:46 +0300

    default.html

--- models/send_m.php
+++ models/send_m.php
@@ -41,4 +41,52 @@ class Send_m extends Model {
         return $this->db->query($sql);
     }
 
+
+    // Save to table messages - Заявки
+    public function save($data, $id = null){
+        if ( !isset($data['name']) || !isset($data['phone']) || !isset($data['title']) ){
+            return false;
+        }
+
+        // delete 'space';
+        $data = $this->db->trimAll_l($data);
+
+        $id = (int)$id;
+        $name = $this->db->escape($data['name']);
+        $phone = $this->db->escape($data['phone']);
+        $title = $this->db->escape($data['title']);
+
+
+
+        if ( !$id ){ // Add new record
+            $sql = "
+                insert into `messages`
+                   set name = '{$name}',
+                       phone = '{$phone}',
+                       title = '{$title}'
+
+            ";
+
+
+        } else { // Update existing record
+            $sql = "
+                update `messages`
+                   set name = '{$name}',
+                       phone = '{$phone}',
+                       title = '{$title}'
+
+                   where `id` = {$id}
+            ";
+        }
+
+        return $this->db->query($sql);
+    }
+
+    // Delete from table messages
+    public function delete($id){
+        $id = (int)$id;
+        $sql = "delete from `messages` where `id` = {$id}";
+        return $this->db->query($sql);
+    }
+
 }
