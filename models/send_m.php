72bcadb055a8968c31733dd69086097dc3350914
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-28 17:48:38 +0300

    default.html

--- models/send_m.php
+++ models/send_m.php
@@ -4,25 +4,28 @@ class Send_m extends Model {
 
     //  Send message
     public function SendMsg($data, $id = null){
-        if ( !isset($data['name']) || !isset($data['phone']) ){
+        if ( !isset($data['name']) || !isset($data['phone']) || !isset($data['title'])){
             return false;
         }
 
         $id = (int)$id;
         $name = $this->db->escape($data['name']);
         $phone = $this->db->escape($data['phone']);
+        $title = $this->db->escape($data['title']);
 
         if ( !$id ){ // Add new record
             $sql = "
                 insert into `messages`
                    set name = '{$name}',
-                       phone = '{$phone}'
+                       phone = '{$phone}',
+                       title = '{$title}'
             ";
         } else { // Update existing record
             $sql = "
                 update `messages`
                    set name = '{$name}',
-                       phone = '{$phone}'
+                       phone = '{$phone}',
+                       title = '{$title}'
                    where id = {$id}
             ";
         }
