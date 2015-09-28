56fa55d4b24ac70554d9d077126e061c9eb176a0
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-28 23:03:19 +0300

    default.html

--- models/send_m.php
+++ models/send_m.php
@@ -4,32 +4,12 @@ class Send_m extends Model {
 
     public function sendEmail(){
 
-        $mail = new PHPMailer;
-
-        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-        // $mail->isSMTP();                                      // Set mailer to use SMTP
-        // $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-        // $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        // $mail->Username = 'user@example.com';                 // SMTP username
-        // $mail->Password = 'secret';                           // SMTP password
-        // $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        // $mail->Port = 587;                                    // TCP port to connect to
-
-        $mail->setFrom('order@salon-ss.com.ua', 'Order');
-        $mail->addAddress('oleg.nasteka@gmail.com', 'Oleg Nasteka');     // Add a recipient
-        // $mail->addAddress('ellen@example.com');               // Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
-
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-        $mail->isHTML(true);                                  // Set email format to HTML
-
-        $mail->Subject = 'Новый заказ';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail = new PHPMailer();
+        $mail->From = 'order@salon-ss.com.ua';      // от кого
+        $mail->FromName = 'salon-ss';   // от кого Имя
+        $mail->AddAddress('oleg.nasteka@gmail.com', 'Олег'); // кому Ваша почта, Имя
+        $mail->IsHTML(true);        // формат письма HTML
+        $mail->Subject = "Новый заказ!";  // тема письма
 
         if(!$mail->send()) {
             echo 'Message could not be sent.';
@@ -37,6 +17,7 @@ class Send_m extends Model {
         } else {
             echo 'Message has been sent';
         }
+
 
     }
 
