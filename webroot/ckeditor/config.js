b2b9457425c77eea5aad0f402d013f854d3805c4
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-30 01:38:35 +0300

    default.html

--- webroot/ckeditor/config.js
+++ webroot/ckeditor/config.js
@@ -5,6 +5,29 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+	config.language = 'ru';
 	// config.uiColor = '#AADC6E';
+    config.height = '300px';
+    CKEDITOR.disableAutoInline = true;
+
+    // automatically remove formating
+    CKEDITOR.config.forcePasteAsPlainText = true;
+    CKEDITOR.config.pasteFromWordRemoveStyles = true;
+    CKEDITOR.config.pasteFromWordRemoveFontStyles = true;
+
+    // при нажатии enter добавляем br
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.shiftEnterMode = CKEDITOR.ENTER_BR;
+
+    // отключить  Advanced Content Filter – Automatic Mode //
+    config.allowedContent = true;
+    // отключить  Advanced Content Filter – Automatic Mode //
+
+    // accept all div classes
+    config.extraAllowedContent = 'div(*)';
+    config.extraAllowedContent = 'span(*)';
+
+    CKEDITOR.dtd.$removeEmpty.span = 0;
+
+
 };
