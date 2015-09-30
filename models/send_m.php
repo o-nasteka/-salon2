23d03548d610a6a7c58df90ff3131eca73c06810
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-30 14:29:41 +0300

    default.html

--- models/send_m.php
+++ models/send_m.php
@@ -7,7 +7,10 @@ class Send_m extends Model {
         $mail = new PHPMailer;
 
         $mail->CharSet = 'UTF-8';
+        // $mail->setLanguage('ru');
         $mail->setFrom('order@salon-ss.com.ua', 'Order');
+        $mail->Subject = 'New Order!';
+
         $mail->addAddress('oleg.nasteka@gmail.com', 'Oleg Nasteka');     // Add a recipient
         // $mailer->AddBCC('recipient1@domain.com', 'Первый человек');  Невидимые получатели или BCC
         // $mail->addAddress('ellen@example.com');               // Name is optional
@@ -15,7 +18,7 @@ class Send_m extends Model {
 
         $mail->isHTML(true);                                  // Set email format to HTML
 
-        $mail->Subject = 'New Order!';
+
 
 
         $mess = '
@@ -23,35 +26,7 @@ class Send_m extends Model {
         <strong>Phone: </strong>'.$data['phone'].'<br />
         <strong>Title: </strong>'.$data['title'];
 
-
-        /*
-        $mess =
-'
-        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>New order</title>
-  <title>New order</title>
-</head>
-<body>
-<div style="width: 640px; font-family: Arial, Helvetica, sans-serif; font-size: 11px;">
-  <h3>New order from www.salon-ss.com.ua</h3><br>
-  <div align="left">
-     <p >Name: '.$data['name'].'</p>
-     <p>Phone: '.$data['phone'].'</p>
-  </div>
-
-
-
-</div>
-</body>
-</html>
-        ';
-*/
-
-
-
+        //
         $mail->Body=$mess;
 
         // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
