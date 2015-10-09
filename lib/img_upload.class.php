e5565451484dfd237c52b52a3b3eb2b5af7fa055
serg234 <icolorwave@gmail.com>
2015-10-09 16:25:08 +0300

    cammit

--- lib/img_upload.class.php
+++ lib/img_upload.class.php
@@ -51,87 +51,108 @@ class Img_upload{
 // $name = date('Ymd_His') . 'img' . rand(10000,99999) . $file_ext_dot;
 
 
-public function __construct($path){
+	public function __construct($path){
 
-$this-> set_path($path);	
-$this-> upload();
-
-}
-
-
-
-public function get_path_full(){
-	return $this->path_full;
-}
-
-private function set_path($path){
-	$this->path = $path;
-}
-
-
-
-
-private function upload(){
-
-	// вырезаем разширение файла с точкой
-	$this->file_ext_dot  = strrchr($_FILES['files']['name'],".");
-	   	
-	if(!$this->file_ext_dot){
-		exit('Not correct file');
+		$this-> set_path($path);
+		$this-> upload();
 
 	}
 
-	$tmp_file = $_FILES['files']['name'];
-	$pos = mb_strripos($tmp_file,'.',0,'UTF-8');
-	$this->file_name = $tmp = mb_substr($tmp_file,0,$pos,"UTF-8");
 
-	// вырезаем точку в раширении файла
-	$this->file_ext = mb_substr($this->file_ext_dot,1);
-	$this->file_ext = mb_strtolower($this->file_ext,"UTF-8");
-	   
-	// Проверяем на допустимые разширения 
-	if(!in_array($this->file_ext,$this->array_ext)){
-	    exit('Not support file!');
+
+	public function get_path_full(){
+		return $this->path_full;
 	}
 
-	// Если ошибок нет
-	if($_FILES['files']['error'] == 0){
-			
-		if($_FILES['files']['size'] < $this->size_up){
-	           
-	    	// Получает свойства загружаемой картинки
-	    	$temp = getimagesize($_FILES['files']['tmp_name']);	         
-	        // Проверяем на допустимый mime тип
-	    	if(in_array($temp['mime'],$this->array_mime)){
-
-				// Формирование пути и имени файла
-	        	$this->path_full = $this->path . $this->file_name . '_' . rand(10000,99999) . $this->file_ext_dot;
+	private function set_path($path){
+		$this->path = $path;
+	}
 
 
-	        	// Выгрузить временной файл по сформировавщемуся пути $this->path_full 
-	        	if(move_uploaded_file($_FILES['files']['tmp_name'],$this->path_full)){
-	            	echo 'Upload comlate';
-					Session::setFlash('Upload comlate!!!');
-					
-	            }else{
-					exit('Upload in correct');
-	             }
 
+
+	private function upload(){
+
+		// вырезаем разширение файла с точкой
+		$this->file_ext_dot  = strrchr($_FILES['files']['name'],".");
+
+		if(!$this->file_ext_dot){
+			exit('Not correct file');
+
+		}
+		// Имя файла без разширения
+		$tmp_file = $_FILES['files']['name'];
+		$pos = mb_strripos($tmp_file,'.',0,'UTF-8');
+		$this->file_name = $tmp = mb_substr($tmp_file,0,$pos,"UTF-8");
+		// Имя файла без разширения конец
+
+		// Кир - лат
+		$this->file_name = $this->cyr_lat($this->file_name);
+
+		// вырезаем точку в раширении файла
+		$this->file_ext = mb_substr($this->file_ext_dot,1);
+		$this->file_ext = mb_strtolower($this->file_ext,"UTF-8");
+
+		// Проверяем на допустимые разширения
+		if(!in_array($this->file_ext,$this->array_ext)){
+			exit('Not support file!');
+		}
+
+		// Если ошибок нет
+		if($_FILES['files']['error'] == 0){
+
+			if($_FILES['files']['size'] < $this->size_up){
+
+				// Получает свойства загружаемой картинки
+				$temp = getimagesize($_FILES['files']['tmp_name']);
+				// Проверяем на допустимый mime тип
+				if(in_array($temp['mime'],$this->array_mime)){
+
+					// Формирование пути и имени файла
+					$this->path_full = $this->path . $this->file_name . '_' . rand(10000,99999) . $this->file_ext_dot;
+
+
+					// Выгрузить временной файл по сформировавщемуся пути $this->path_full
+					if(move_uploaded_file($_FILES['files']['tmp_name'],$this->path_full)){
+						echo 'Upload comlate';
+						Session::setFlash('Upload comlate!!!');
+
+					}else{
+						exit('Upload in correct');
+					 }
+
+				}else{
+					exit('mime not correct');
+
+				 }
 			}else{
-				exit('mime not correct');
+				// size byte/1024 = size kbyte;
+				exit('size file not correct');
+			}
 
-			 }
-	    }else{
-	    	// size byte/1024 = size kbyte;
-	        exit('size file not correct');
-	    }
-	        
-	      
-	 }else{
-	    exit('errors upload files');
-	 }
 
-}
+		 }else{
+			exit('errors upload files');
+		 }
+
+	}
+
+
+		private function cyr_lat($textcyr = null, $textlat = null) {
+			$cyr = array(
+				'ж',  'ч',  'щ',   'ш',  'ю',  'а', 'б', 'в', 'г', 'д', 'е', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ъ', 'ь', 'я',
+				'Ж',  'Ч',  'Щ',   'Ш',  'Ю',  'А', 'Б', 'В', 'Г', 'Д', 'Е', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ъ', 'Ь', 'Я');
+			$lat = array(
+				'zh', 'ch', 'sht', 'sh', 'yu', 'a', 'b', 'v', 'g', 'd', 'e', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'y', 'x', 'q',
+				'Zh', 'Ch', 'Sht', 'Sh', 'Yu', 'A', 'B', 'V', 'G', 'D', 'E', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'c', 'Y', 'X', 'Q');
+			if($textcyr) return str_replace($cyr, $lat, $textcyr);
+			else if($textlat) return str_replace($lat, $cyr, $textlat);
+			else return null;
+		}
+
+
+
+
 }
 
 
