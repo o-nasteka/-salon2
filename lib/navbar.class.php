2789ce8e761f43b3372f37452000f3f48f167237
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-10-09 17:40:00 +0300

    send_m

---
+++ lib/navbar.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Top Menu
+ * User: nasteka
+ * Date: 09.10.15
+ * Time: 17:32
+ */
+
+class NavBar {
+
+    public static function TopMenu(){
+
+        $template_name = "navbar.html";
+        return VIEWS_PATH.DS.$template_name;
+
+    }
+
+}
+
+
+
+
+?>
