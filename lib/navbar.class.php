f951f5b632570363c60a1fd0a00d10e38f1da96f
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-10-09 19:47:10 +0300

    send_m

--- lib/navbar.class.php
+++ lib/navbar.class.php
@@ -6,17 +6,18 @@ class NavBar {
     public static function TopMenu(){
 
         // Get Menu
-        $data['menu'] = self::getMenu();
+        $menu = self::getMenu();
 
-        $template_name = VIEWS_PATH.DS."navbar.php";
-        return require_once("$template_name");
+        $template_name = VIEWS_PATH.DS."navbar.html";
+        // var_dump($template_name);
+        // exit;
+        return require("$template_name");
 
     }
 
     // getMenu
     private static function getMenu(){
-        $sql = "select * from `products` ";
-
+        $sql = "select `alias`,`title`,`parent_id` from `products` ";
         return App::$db->query($sql);
     }
 
