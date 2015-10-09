a35fdfd32a5ab1aacae224d82b4502d9ac128add
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-10-09 20:37:06 +0300

    send_m

--- webroot/ckeditor/config.js
+++ webroot/ckeditor/config.js
@@ -36,4 +36,13 @@ CKEDITOR.editorConfig = function( config ) {
     CKEDITOR.dtd.$removeEmpty.span = 0;
 
 
+    config.filebrowserBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=files';
+    config.filebrowserImageBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=images';
+    config.filebrowserFlashBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=flash';
+    config.filebrowserUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=files';
+    config.filebrowserImageUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=images';
+    config.filebrowserFlashUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=flash';
+
+
+
 };
