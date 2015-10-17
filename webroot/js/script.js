44dfdbb855757b0588cdef51e51627d3359824d8
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-10-17 20:05:24 +0300

    send_m

--- webroot/js/script.js
+++
@@ -1,7 +0,0 @@
-var map = new google.maps.Map( document.getElementById( 'map-canvas' ), {
-
-    zoom:         7,
-    center:       new google.maps.LatLng( originalLocation[0], originalLocation[1] ),
-    scrollwheel:  false
-
-});
