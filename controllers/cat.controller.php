bd954a51e110e021cc0ed52a211c238dcf1a227d
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-10-21 12:29:07 +0300

    send_m

--- controllers/cat.controller.php
+++ controllers/cat.controller.php
@@ -14,19 +14,19 @@ class CatController extends Controller{
             @$_SESSION['sort'] = @$_POST['sort'];
         }
         if (!isset($_POST['sort']) && !isset($_SESSION['sort'])) {
-            $this->data['products'] = $this->model->getList();
+            $this->data['cat'] = $this->model->getList();
         } elseif ($_SESSION['sort'] == 1) {
-            $this->data['products'] = $this->model->getList_jaluzi();
+            $this->data['cat'] = $this->model->getList_jaluzi();
         } elseif ($_SESSION['sort'] == 2) {
-            $this->data['products'] = $this->model->getList_roleti();
+            $this->data['cat'] = $this->model->getList_roleti();
         } elseif ($_SESSION['sort'] == 3) {
-            $this->data['products'] = $this->model->getList_plisse();
+            $this->data['cat'] = $this->model->getList_plisse();
         } elseif ($_SESSION['sort'] == 4) {
-            $this->data['products'] = $this->model->getList_antimos();
+            $this->data['cat'] = $this->model->getList_antimos();
         } elseif ($_SESSION['sort'] == 5) {
-            $this->data['products'] = $this->model->getList_out_sys();
+            $this->data['cat'] = $this->model->getList_out_sys();
         } elseif ($_SESSION['sort'] == 6) {
-            $this->data['products'] = $this->model->getList();
+            $this->data['cat'] = $this->model->getList();
         }
 
     }
--- models/cat_m.php
+++ models/cat_m.php
@@ -10,6 +10,58 @@ class Cat_m extends Model{
         // $id = (int)$id;
         return $this->db->query($sql);
     }
+
+    public function getList_jaluzi(){
+        // $sql = "SELECT * FROM `categories`";
+        $sql = "SELECT * FROM `categories` WHERE `id` IN (2,6,10) ";
+
+        // $id = (int)$id;
+        return $this->db->query($sql);
+    }
+
+    public function getList_roleti(){
+        // $sql = "SELECT * FROM `categories`";
+        $sql = "SELECT * FROM `categories` WHERE `id` IN (14,21,26,32,31) ";
+
+        // $id = (int)$id;
+        return $this->db->query($sql);
+    }
+
+    public function getList_plisse(){
+        // $sql = "SELECT * FROM `categories`";
+        $sql = "SELECT * FROM `categories` WHERE `id` = 33 ";
+
+        // $id = (int)$id;
+        return $this->db->query($sql);
+    }
+
+    public function getList_antimos(){
+        // $sql = "SELECT * FROM `categories`";
+        $sql = "SELECT * FROM `categories` WHERE `id` = 38 ";
+
+        // $id = (int)$id;
+        return $this->db->query($sql);
+    }
+
+    public function getList_out_sys(){
+        // $sql = "SELECT * FROM `categories`";
+        $sql = "SELECT * FROM `categories` WHERE `id` = 49 OR `id` = 50
+        OR `id` = 51 OR `id` = 52 OR `id` = 53 OR `id` = 54   ";
+
+        // $id = (int)$id;
+        return $this->db->query($sql);
+    }
+
+
+    // Delete from table categories
+    public function delete($id){
+        $id = (int)$id;
+        $sql = "delete from `categories` where `id` = {$id}";
+        return $this->db->query($sql);
+    }
+
+
+
 }
 
 ?>
